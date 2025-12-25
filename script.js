a14375018a66a33dbfa8e1cd07d4f3fd63521c6b
k70118088-star <k70118088@gmail.com>
2025-12-25 10:11:41 +0530

    updated

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
             {
                 breakpoint: 768,
                 settings: {
-                    dots: false
+                    dots: true
                 }
             }
         ]
